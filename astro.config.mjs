8b00b66a073679a2c7f8555d1224b703624752f4
Ichem <ichem30@gmail.com>
2025-09-15 20:57:26 +0200

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ichem30.github.io',
-  base: '/site', // Nom de votre repository
-  output: 'static'
+  base: '/site',
+  output: 'static',
+  build: {
+    assets: '_astro'
+  }
 });
