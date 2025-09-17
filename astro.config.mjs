cede13b62b6a021ff7fd1a1107db9db2fbd72735
Ichem <ichem30@gmail.com>
2025-09-17 21:21:29 +0200

    opti  mailgun

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://ichem30.github.io',
-  base: '/site',
-  output: 'static',
-  build: {
-    assets: '_astro'
-  }
-});
+export default defineConfig({});
